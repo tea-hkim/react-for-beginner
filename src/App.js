9672bdc396ecec632e93ccb8e4b1794881371f10
im_TaeHo_K <xogh8025@naver.com>
2022-01-14 19:28:26 +0900

    feat Basic Coin Tracker 0.0.2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Button from "./Button";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coinList, setCoinList] = useState([]);
+  const [dollar, setDollar] = useState("");
+  const handleDollarChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setDollar(value);
+  };
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -13,20 +21,32 @@ function App() {
         setLoading(false);
       });
   }, []);
+
   return (
     <div className="App">
       <h1>This is Coin Tracker!({coinList.length})</h1>
       {loading ? <strong>"Loading..."</strong> : null}
       <hr />
-      <ul>
+      <input
+        value={dollar}
+        onChange={handleDollarChange}
+        type="number"
+        placeholder=""
+      />
+      <h2>
+        You can get {coinList[0] ? dollar / coinList[0].quotes.USD.price : null}
+        BTC for {dollar}dollars
+      </h2>
+      <br />
+      <select>
         {coinList.map((coin) => {
           return (
-            <li key={coin.id}>
+            <option key={coin.id}>
               {coin.name}({coin.symbol}) : {coin.quotes.USD.price} USD
-            </li>
+            </option>
           );
         })}
-      </ul>
+      </select>
     </div>
   );
 }
